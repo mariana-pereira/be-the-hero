2bdfa8adc1da6b24e5f46bdd7356d81e28ff5cd2
Mariana <marianna.22sp@gmail.com>
2020-03-24 13:36:56 -0300

    backend finalizado

---
+++ node/src/routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const OngController = require('./app/controllers/OngController');
+const IncidentController = require('./app/controllers/IncidentController');
+const ProfileController = require('./app/controllers/Profile.controller');
+const SessionController = require('./app/controllers/SessionController');
+
+const routes = Router();
+
+routes.post('/sessions', SessionController.store);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
